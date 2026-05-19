b1b2753249053fe0ad17e8065ede0730e8cccbeb
LucasMachuca <lucasamachuca07@gmail.com>
2026-05-19 10:24:45 -0300

    Actualizacion: servidor.py se encarga de reenviar los mensajes, cliente1.py ahora puede recibir y enviar mensaje en paralelo y se elimino cliente2.py

--- cliente1.py
+++ cliente1.py
@@ -1,26 +1,53 @@
 from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
 
 HOST = "127.0.0.1"
 PORT = 5000
 
+nombre = input ("Nombre de usuario: ")
+if nombre == "":
+    nombre = "anonimo"
+
+#Creo el socket
 cliente = socket(AF_INET, SOCK_STREAM)
 
 #Me conecto al servidor pasandole la direccion y puerto que utilizaremos 
 cliente.connect ((HOST, PORT))
 
-#Recibimos el mensaje que envia el servidro 
-while True:
-    mensaje_servidor_bytes = cliente.recv (1024)   
-    mensaje_servidor = mensaje_servidor_bytes.decode ("utf-8")
-    print (f"Servidor: {mensaje_servidor}")
+#Le envio el nombre como primer mensaje al servidor luego entra en el loop
+nombre_bytes = nombre.encode ("utf-8")
+cliente.send (nombre_bytes)
 
-    #Mandamos mensaje al servidor 
-    mensaje_cliente = input ("Cliente_1: ")
+def recibir_mensaje ():
+    while True:
+        mensaje_bytes= cliente.recv (1024)
 
-    if mensaje_cliente == "":
-        cliente.close ()
-        break
-    
-    else:
-        mensaje_cliente_bytes  = mensaje_cliente.encode ("utf-8")
-        cliente.send (mensaje_cliente_bytes)
+        if not mensaje_bytes:
+            cliente.close ()
+            print ("Un usuario abandono el chat")
+            break
+
+        mensaje = mensaje_bytes.decode ("utf-8")
+        print (f"\nServidor: {mensaje}\n usuario: ", end="", flush=True)
+ 
+def enviar_mensaje ():
+    while True :
+        mensaje = input ("")
+
+        if mensaje == "":
+            cliente.close ()
+            print ("Se abandondo el chat ")
+            break
+        
+        mensaje_bytes = mensaje.encode ("utf-8")
+        cliente.send (mensaje_bytes)
+
+
+#Creamos los hilos e Iniciamos 
+hilo_enviar = Thread (target= enviar_mensaje)
+hilo_recibir = Thread (target= recibir_mensaje)
+
+hilo_enviar.start ()
+hilo_recibir.start ()
+
+
--- servidor.py
+++ servidor.py
@@ -1,28 +1,42 @@
 from socket import *
 from threading import Thread
 
+#Lista que nos ayudara a guardar a los clientes que se van uniendo
+clientes = []
+
+def broadcast (mensaje:str, cliente_emisor:socket):
+
+    mensaje_bytes = mensaje.encode ("utf-8")
+
+    for cliente in clientes:
+
+        if not cliente == cliente_emisor:
+            cliente.send (mensaje_bytes)
+
+
 #Funcion que se encarga de recibir el mensaje de los clientes
 def manejar_cliente (cliente_socket:socket, cliente_address:tuple):
 
-    while True:       
+    while True:     
+        #Rcibe mensaje del cliente  
         mensaje_cliente_bytes = cliente_socket.recv (1024)
 
+        #Sino recibio un mensaje entonces cierra el bucle y cierra el socket 
         if not mensaje_cliente_bytes:
+            #Removemos al cliente de la lista y cerramos el socket
+            clientes.remove (cliente_socket)
             cliente_socket.close ()
-            print ("Se deconecto el cliente ")
+            print (f"Se deconecto el cliente [{cliente_address[0]}:{cliente_address[1]}]")
             break
-    
+        
+        #Imprimimos el mensaje del cliente
         mensaje_cliente = mensaje_cliente_bytes.decode ("utf-8")
-        print (f"[{cliente_address[0]}:{cliente_address[1]}]: {mensaje_cliente}")
+        mensaje_id = f"[{cliente_address[0]}: {cliente_address[1]}]: {mensaje_cliente}"
+        print (f"[{cliente_address[0]}:{cliente_address[1]}]: {mensaje_id}")
 
-        mensaje_servidor = input (f"responde a [{cliente_address[0]}:{cliente_address[1]}]: ")
-        if mensaje_servidor =="":
-            print (f"Se cerro el chat con el cliente [{cliente_address[0]}:{cliente_address[1]}]")
-            cliente_socket.close ()
-            break
-        else:    
-            mensaje_bytes_servidor = mensaje_servidor.encode ()
-            cliente_socket.send (mensaje_bytes_servidor)
+        #Llamo a la funcion que se encargara de reenviar los mensajes 
+        broadcast (mensaje_cliente, cliente_socket)
+
 
 
 #Creamos el socket y le decimos:
@@ -40,13 +54,17 @@ while True:
 
     #Aceptamos al cliente y recibos el mensaje como su direccion.
     cliente_socket, cliente_address = server.accept ()
-    print (f"""\nEl cliente se conecto desde:
-    ID: {cliente_address[0]}
-    Puerto: {cliente_address[1]}\n""")
+    clientes.append (cliente_socket)
+    nombre_bytes = cliente_socket.recv (1024)
+    nombre_cliente = nombre_bytes.decode ("utf-8")
+
+    print (f"""\n{nombre_cliente} se conecto desde:
+ID: {cliente_address[0]}
+Puerto: {cliente_address[1]}\n""")
 
     #Mensaje de bienvenida al cliente 
-    mensaje_servidor = "Hola Bienvenido al chat "
-    mensaje_bytes_servidor = mensaje_servidor.encode ("utf-8")
-    cliente_socket.send (mensaje_bytes_servidor)
+    # mensaje_servidor = "Hola Bienvenido al chat "
+    # mensaje_bytes_servidor = mensaje_servidor.encode ("utf-8")
+    # cliente_socket.send (mensaje_bytes_servidor)
     hilo = Thread (target=manejar_cliente, args=(cliente_socket, cliente_address))
     hilo.start ()
