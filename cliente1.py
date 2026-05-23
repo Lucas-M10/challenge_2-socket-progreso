deab49ac6397bb4793b5312dbd3bab90865c5afa
Lucas-M10 <lucasamachuca07@gmail.com>
2026-05-23 16:49:19 -0300

    Actualizacion: Se modifico cliente1.py ahora es mas seguro ante fallas pudiendo manejar excepciones

--- cliente1.py
+++ cliente1.py
@@ -6,43 +6,80 @@ PORT = 5000
 
 nombre = input ("Nombre de usuario: ")
 if nombre == "":
-    nombre = "anonimo"
+    nombre = "Anonimo"
+
 
 #Creo el socket
 cliente = socket(AF_INET, SOCK_STREAM)
 
 #Me conecto al servidor pasandole la direccion y puerto que utilizaremos 
-cliente.connect ((HOST, PORT))
+try:
+    cliente.connect ((HOST, PORT))
+
+except ConnectionRefusedError:
+    print (f"No se pudo conectar al servidor. Verificar que el servidor este activo")
+    exit()
+
 
 #Le envio el nombre como primer mensaje al servidor luego entra en el loop
-nombre_bytes = nombre.encode ("utf-8")
-cliente.send (nombre_bytes)
+try:
+    nombre_bytes = nombre.encode ("utf-8")
+    cliente.send (nombre_bytes)
+
+except (ConnectionError, OSError) as error:
+    print(f"No se pudo enviar el nombre al servidor: \n{error}")
+
+#Funcion que se encarga de desconectar correcctamente
+def desconectar_cliente ():
+
+    try:
+        cliente.close ()
+
+    except OSError as error:
+        print (f"{error}: al intentar desconectar al cliente")
+
 
 #Funcion que se encarga de recibir los mensajes e imprimir en pantalla 
 def recibir_mensaje ():
+
     while True:
-        mensaje_bytes= cliente.recv (1024)
+        try:
+            mensaje_bytes= cliente.recv (1024)
 
-        if not mensaje_bytes:
-            cliente.close ()
-            print ("Un usuario abandono el chat")
+        except (ConnectionResetError, OSError) as error:
+            print(f"Error al recibir el mensaje: \n{error}")
+            desconectar_cliente ()
             break
+        
+        else:
 
-        mensaje = mensaje_bytes.decode ("utf-8")
-        print (f"\n{mensaje}\n usuario: ", end="", flush=True)
+            if not mensaje_bytes:
+                desconectar_cliente()
+                break
+
+            mensaje = mensaje_bytes.decode ("utf-8")
+            print (f"\n{mensaje}\n{nombre}: ", end="", flush=True)
  
 #Funcion que se encarga de enviar los mensajes 
 def enviar_mensaje ():
+
     while True :
         mensaje = input ("")
 
         if mensaje == "":
-            cliente.close ()
             print ("Se abandondo el chat ")
+            desconectar_cliente ()
             break
         
         mensaje_bytes = mensaje.encode ("utf-8")
-        cliente.send (mensaje_bytes)
+
+        try:
+            cliente.send (mensaje_bytes)
+
+        except (BrokenPipeError, ConnectionResetError, OSError) as error:
+            print (f"Error al enviar mensaje: \n{error}")
+            desconectar_cliente ()
+            break
 
 
 #Creamos los hilos e Iniciamos 
@@ -51,5 +88,3 @@ hilo_recibir = Thread (target= recibir_mensaje)
 
 hilo_enviar.start ()
 hilo_recibir.start ()
-
-
